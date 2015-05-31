7959b060ff8517726e19b3978d5fcf43d2d1cc6d
Matt Staib <mstaib@stanford.edu>
2015-05-31 09:34:22 -0700

    split out problem definition

--- ono.jl
+++ ono.jl
@@ -7,50 +7,7 @@
     Implementation of Ono et. al. CCPD algorithm
 =#
 
-using PyCall
-@pyimport scipy.stats.mvn as mvn
-mvnun = convert(Function, mvn.mvnun)
-
-type ChanceConstrainedProblem
-    # Val at each point is false if in bounds else true
-    grid::Array{Bool,2}
-    g_N::Function
-    g_k::Function
-    Δ::Float64
-    #f::Function
-
-    σ²::Float64 #noise
-    d::Float64 #controller bound
-end
-
-function prob_of_this_square(x, center, σ²)
-    return mvnun(x - [0.5, 0.5], x + [0.5, 0.5], center, σ²*eye(2))[1]
-end
-
-#probs_cache = (Tuple{Array{Bool,2},Float64} => Array)[]
-probs_cache = (Any => Array)[]
-function get_probs(prob, x)
-    h, w = size(prob.grid)
-
-    tup = (prob.grid, prob.σ²)
-
-    if haskey(probs_cache, tup)
-        probs_big = probs_cache[tup]
-    else
-        # this is expensive
-        probs_big = zeros(2*h-1, 2*w-1)
-        for i = 1:2*h-1
-            for j = 1:2*w-1
-                probs_big[i, j] = prob_of_this_square([i, j], [h, w], prob.σ²)
-            end
-        end
-
-        probs_cache[tup] = probs_big
-    end 
-
-    i, j = x
-    return probs_big[1+h-i:h+h-i, 1+w-j:w+w-j]
-end
+require("chance_constrained_problem.jl")
 
 # returns ??
 function ono_dp(prob, λ, N)
@@ -118,21 +75,6 @@ function ono_dp(prob, λ, N)
     return J
 end
 
-function build_test_prob(dim)
-    grid_int = zeros(dim,dim)
-    grid_int[3:4,3:4] = 1
-    grid = convert(Array{Bool}, grid_int)
-
-    g_N(k, x) = x == [dim,dim]? 0 : 1
-    g_k(k, x, u) = 0.0001 * norm(u)
-    Δ = 0.2
-    σ² = 1
-    d = 2
-
-    prob = ChanceConstrainedProblem(grid, g_N, g_k, Δ, σ², d)
-
-    return prob
-end
 
 function test_ono_dp(λ, dim)
     N = 50
