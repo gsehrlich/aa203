fa244633025ef0384cd50e6de2138c928da03a12
Matt Staib <mstaib@stanford.edu>
2015-06-03 00:01:19 -0700

    read in all monte carlo data

--- simulate_lambdas.jl
+++ simulate_lambdas.jl
@@ -4,7 +4,7 @@ require("test_probs.jl")
 using HDF5, JLD
 
 #data_dir = error("change this to point to your data")
-data_dir = "../lambda_data/lambda_data"
+data_dir = "lambda_data"
 
 function monte_carlo_all_lambdas(num_samples)
     failure_rate_λ = (Any => Any)[]
@@ -35,4 +35,28 @@ function noiseless_path_lambdas()
         plot_noiseless_path(interesting_test_prob, μ,
                             interesting_test_x0, 10, color=color)
     end
+end
+
+function monte_carlo_lambdas()
+    monte_carlo_dir = "monte_carlo_lambda"
+    filenames = readdir(monte_carlo_dir)
+
+    #magic numbers!!!
+    λs = sort([float(filename[29:end]) for filename in filenames])
+    pairs = sort([zip(λs, filenames)...])
+    n = length(filenames)
+
+    failure_rates = Real[]
+
+    for (i, (λ, filename)) in enumerate(pairs)
+        println("λ = $(λ)")
+        failure_rate = load("$(monte_carlo_dir)/$(filename)")["failure_rate"]
+        push!(failure_rates, failure_rate)
+
+        #color = (i/n, 0, 1 - i/n)
+        #plot_noiseless_path(interesting_test_prob, μ,
+        #                    interesting_test_x0, 10, color=color)
+    end
+
+    return λs, failure_rates
 end
